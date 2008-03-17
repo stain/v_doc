a71322ac128948422a6aa448a1119346bfdc6233
ktk <ktk@985fc4c0-a511-0410-a4be-e915bfa2b0ec>
2008-03-17 23:03:26 +0000

    Mindmap import

---
+++ concepts/Voyager.mm
@@ -0,0 +1,55 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1205182520463" ID="Freemind_Link_594631965" MODIFIED="1205182526590" TEXT="Voyager">
+<node CREATED="1205702359585" ID="Freemind_Link_1092357633" MODIFIED="1205702366641" POSITION="left" TEXT="Lifecycle management">
+<node CREATED="1205702394039" ID="Freemind_Link_1233185312" MODIFIED="1205702418867" TEXT="Quality asurance"/>
+<node CREATED="1205702420178" ID="Freemind_Link_891949883" MODIFIED="1205702466186" TEXT="Bugtracker"/>
+<node CREATED="1205702368275" ID="Freemind_Link_1023408132" MODIFIED="1205702389406" TEXT="Release maintenance"/>
+</node>
+<node CREATED="1205689888495" ID="_" MODIFIED="1205690760367" POSITION="left" TEXT="Project organization">
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>lksjdlsakjdlsadk</text>
+</hook>
+<node CREATED="1205689969989" ID="Freemind_Link_1496673818" LINK="http://vwiki.netlabs.org/index.php/Project_Roles" MODIFIED="1205690915291" TEXT="Charta/Project Roles"/>
+<node CREATED="1205182568413" ID="Freemind_Link_1429768917" MODIFIED="1205690238821" TEXT="Core Team">
+<node CREATED="1205689899786" ID="Freemind_Link_912000982" MODIFIED="1205691254002" TEXT="Its interface to the public"/>
+<node CREATED="1205690192817" ID="Freemind_Link_1660113626" MODIFIED="1205690274854" TEXT="Internal setup"/>
+</node>
+<node CREATED="1205690841282" ID="Freemind_Link_1453499083" MODIFIED="1205690979085" TEXT="Public members"/>
+</node>
+<node CREATED="1205702503209" ID="Freemind_Link_128777754" MODIFIED="1205702506638" POSITION="right" TEXT="Security"/>
+<node CREATED="1205622977283" ID="Freemind_Link_755126748" MODIFIED="1205623021956" POSITION="right" TEXT="Singleness">
+<node CREATED="1205623046413" ID="Freemind_Link_1821290494" MODIFIED="1205623059545" TEXT="Beyond the Browser">
+<node CREATED="1205623061737" ID="Freemind_Link_1153720685" MODIFIED="1205623185835" TEXT="Don&apos;t do applications in a browser"/>
+<node CREATED="1205623116231" ID="Freemind_Link_1381166770" MODIFIED="1205623326509" TEXT="Service providing"/>
+<node CREATED="1205623125936" ID="Freemind_Link_1086049121" MODIFIED="1205623174196" TEXT="Server based"/>
+<node CREATED="1205623375992" ID="Freemind_Link_580580373" MODIFIED="1205623395316" TEXT="Intelligence in objects"/>
+</node>
+<node CREATED="1205623194328" ID="Freemind_Link_666176727" MODIFIED="1205623202922" TEXT="Language independent">
+<node CREATED="1205623399499" ID="Freemind_Link_1863483858" MODIFIED="1205623408248" TEXT="Beyond Java/.net"/>
+<node CREATED="1205623409665" ID="Freemind_Link_841360721" MODIFIED="1205623426859" TEXT="Scripting languages for RAD/prototyping"/>
+</node>
+<node CREATED="1205623223401" ID="Freemind_Link_116282492" MODIFIED="1205623229333" TEXT="Rendering">
+<node CREATED="1205623229264" ID="Freemind_Link_541825459" MODIFIED="1205623273251" TEXT="NOM renderer"/>
+<node CREATED="1205623274239" ID="Freemind_Link_1414379189" MODIFIED="1205623323378" TEXT="No hard renderer "/>
+<node CREATED="1205691091857" ID="Freemind_Link_119232638" MODIFIED="1205691106177" TEXT="RPC interface">
+<node CREATED="1205691121203" ID="Freemind_Link_396466973" MODIFIED="1205691128590" TEXT="XUL"/>
+</node>
+</node>
+<node CREATED="1205623547712" ID="Freemind_Link_1765902495" MODIFIED="1205623553351" TEXT="OpenDoc like?">
+<node CREATED="1205690053005" ID="Freemind_Link_40063992" MODIFIED="1205690064501" TEXT="Objects instead of applications"/>
+<node CREATED="1205690089985" ID="Freemind_Link_1951670904" MODIFIED="1205690166851" TEXT="User creats its system instead of application"/>
+</node>
+<node CREATED="1205623554337" ID="Freemind_Link_1954677331" MODIFIED="1205623616279" TEXT="Semantic web of objects"/>
+</node>
+<node CREATED="1205623894827" ID="Freemind_Link_1186871671" MODIFIED="1205623900426" POSITION="left" TEXT="The Customer"/>
+<node CREATED="1205623815675" ID="Freemind_Link_1570763043" MODIFIED="1205623832571" POSITION="left" TEXT="The Product">
+<node CREATED="1205623832490" ID="Freemind_Link_543325065" MODIFIED="1205623837256" TEXT="A desktop"/>
+<node CREATED="1205623839876" ID="Freemind_Link_272409178" MODIFIED="1205623851614" TEXT="A framework"/>
+</node>
+<node CREATED="1205623869530" ID="Freemind_Link_1178034101" MODIFIED="1205623871815" POSITION="right" TEXT="Vision">
+<node CREATED="1205623874261" ID="Freemind_Link_1442193932" MODIFIED="1205623876066" TEXT="JFK"/>
+</node>
+<node CREATED="1205623922237" ID="Freemind_Link_1041240538" MODIFIED="1205623936029" POSITION="left" TEXT="The Investor"/>
+</node>
+</map>
