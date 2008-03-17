80c3bc83fd74b504c10c183085e9421aef49dade
ktk <ktk@985fc4c0-a511-0410-a4be-e915bfa2b0ec>
2008-03-17 23:03:26 +0000

    Mindmap import

---
+++ doc/concepts/Voyager Pilot.mm
@@ -0,0 +1,38 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1205791922214" ID="Freemind_Link_768171201" MODIFIED="1205793912785" TEXT="Voyager Pilot">
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1205791969979" ID="_" MODIFIED="1205793922548" POSITION="right" TEXT="Bookmarks">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node CREATED="1205793167923" ID="Freemind_Link_1476252844" MODIFIED="1205793651640" TEXT="Microformats">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1205793524986" ID="Freemind_Link_665593530" MODIFIED="1205794513689" TEXT="make  Information usable &#xa;which is not visible now">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node CREATED="1205793177575" ID="Freemind_Link_1746760703" MODIFIED="1205793658300" TEXT="Storage">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1205793411015" ID="Freemind_Link_1286375912" MODIFIED="1205793549852" TEXT="Server Side"/>
+<node CREATED="1205793420781" ID="Freemind_Link_845774019" MODIFIED="1205793545668" TEXT="Local System">
+<node CREATED="1205793550771" ID="Freemind_Link_1828234152" MODIFIED="1205794413256" TEXT="Mozilla Bookmarks">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1205793562315" ID="Freemind_Link_1139165299" MODIFIED="1205794553181" TEXT="other sources..">
+<icon BUILTIN="full-7"/>
+</node>
+</node>
+</node>
+<node CREATED="1205793213259" ID="Freemind_Link_1238828824" MODIFIED="1205794504980" TEXT="Device Context&#xa;Sensitive Information">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1205793471687" ID="Freemind_Link_58711173" MODIFIED="1205793499130" TEXT="Contact Information for mobile phones"/>
+<node CREATED="1205793500665" ID="Freemind_Link_1682402906" MODIFIED="1205793511534" TEXT="Address Information for Navigation Devices"/>
+</node>
+</node>
+<node CREATED="1205793855083" FOLDED="true" ID="Freemind_Link_1430690833" MODIFIED="1205793969969" POSITION="left" TEXT="build bridge to future concept">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="14"/>
+<node CREATED="1205794090536" ID="Freemind_Link_259156336" MODIFIED="1205794121318" TEXT="support of devices">
+<node CREATED="1205794123138" ID="Freemind_Link_347548878" MODIFIED="1205794165609" TEXT="capable of displaying all/certain parts of provided information"/>
+</node>
+</node>
+</node>
+</map>
